a10ab57db4cbdd65d9cad3351cd95a2919dcf091
Carles Núñez Tomeo <carles.nunez@scmspain.com>
2019-01-30 15:19:43 +0100

    Fixed some code unused

--- src/code/contextFile.js
+++ src/code/contextFile.js
@@ -7,11 +7,6 @@ export const Context = ContextLocal;
 
 export const withContext = (Component) => {
     return (props) => {
-        // IDK why do we get am undefined component here
-        if(!Component) {
-            return null;
-        }
-        console.log('----------------->333',Component);
         return (
             <ContextLocal.Consumer>
                 {context => <Component {...props} context={context} />}
